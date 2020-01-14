da0cd1507f08b1500d209cff71c61897c953a5f2
Maksim Kostromin <daggerok@gmail.com>
2020-01-15 00:04:46 +0200

    NOJIRA: No cry (C) whatthecommit.com

---
+++ src/test/java/com/github/daggerok/reactor_04/Part03StepVerifierTests.java
@@ -0,0 +1,35 @@
+package com.github.daggerok.reactor_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class Part03StepVerifierTests {
+
+    @Test
+    void test_05() {
+        StepVerifier.create(Mono.just(Collections.singletonMap("ololo", "trololo")))
+                    .assertNext(stringStringMap -> assertThat(stringStringMap.get("ololo")).isEqualTo("trololo"))
+                    .verifyComplete();
+    }
+
+    @Test
+    void test_06_time_manipulation() {
+        Supplier<Flux<Integer>> supplier = () -> Flux.range(0, 3600)
+                                                     .delayElements(Duration.ofSeconds(1));
+        StepVerifier.withVirtualTime(supplier)
+                    .expectSubscription()
+                    .thenAwait(Duration.ofSeconds(3600))
+                    .expectNextCount(3600)
+                    .verifyComplete();
+    }
+}
