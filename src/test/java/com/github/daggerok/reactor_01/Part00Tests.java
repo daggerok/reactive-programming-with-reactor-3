d2ad178319fd88f306f0f56b324ba4e69cee83ff
Maksim Kostromin <daggerok@gmail.com>
2020-01-14 23:48:42 +0200

    I made leetle mistake (C) whatthecommit.com

--- src/test/java/com/github/daggerok/reactor_01/Part00Tests.java
+++ src/test/java/com/github/daggerok/reactor_01/Part00Tests.java
@@ -3,7 +3,12 @@ package com.github.daggerok.reactor_01;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Part00Tests {
@@ -28,6 +33,13 @@ class Part00Tests {
         flux.map(s -> "foo" + s);
         StepVerifier.create(flux)
                     .expectNext("A")
+                    .verifyComplete();
+    }
+
+    @Test
+    void test_05() {
+        StepVerifier.create(Mono.just(Collections.singletonMap("ololo", "trololo")))
+                    .assertNext(stringStringMap -> assertThat(stringStringMap.get("ololo")).isEqualTo("trololo"))
                     .verifyComplete();
     }
 }
